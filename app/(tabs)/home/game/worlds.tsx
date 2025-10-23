87cc7c786449530e5f6a0ef00aadb35d6307a4df
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2025-10-23 14:45:08 +0300

    Add game background

--- app/(tabs)/home/game/worlds.tsx
+++ app/(tabs)/home/game/worlds.tsx
@@ -1,24 +1,30 @@
 import { router } from "expo-router";
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import { Button, Text, useTheme } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Worlds = () => {
     const theme = useTheme();
 
     return (
-        <SafeAreaView style={styles.container} >
+          <ImageBackground
+              source={require('@/assets/images/WorldNavigation.png')} 
+              style={styles.background}
+              resizeMode="cover"
+          >
             <Text>This is the worlds screen</Text>
             <Text>From here, the user should be able to access different levels / quizzes in the game </Text>
             <Button onPress={() => router.push('/home/game/quiz')} style={styles.button}>
           To the quiz
         </Button>
-        </SafeAreaView>
+        </ImageBackground>
     );
 
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
   container: { 
     flex: 1,
     paddingHorizontal: 20,
