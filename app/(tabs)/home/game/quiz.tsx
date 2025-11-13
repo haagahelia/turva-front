a28257a3a0ca88756b42c27b6ec9adbe4b290e55
Otto Elomaa <otto.elomaa99@gmail.com>
2025-11-13 17:07:17 +0200

    Fetch Quiz: Async fetch done, some initial changes, very broken

--- app/(tabs)/home/game/quiz.tsx
+++ app/(tabs)/home/game/quiz.tsx
@@ -1,5 +1,4 @@
 import { Answer, Language, QuizLang, QuizType } from "@/src/types/types";
-import mock_json from "@/static/w1-s1-act-responsibly.json";
 import { router } from "expo-router";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
@@ -7,26 +6,56 @@ import { ScrollView } from "react-native-gesture-handler";
 import { Button, Text, useTheme } from "react-native-paper";
 import QuizAnswer from "./quiz-answer";
 import QuizQuestion from "./quiz-question";
-import TextData from './textData.json';
+import TextData from "./textData.json";
 
 const Quiz = () => {
 	const theme = useTheme();
-	const lang: Language = 'en'
+	const lang: Language = "en";
 	const commonText = TextData[lang].common;
-	const quizData1 = mock_json as unknown as QuizType;
-	const quizData = quizData1[lang] as QuizLang;
 	const [selectedAnswers, setSelectedAnswers] = useState<Answer[]>([]);
+	const [quizData, setQuizData] = useState<QuizLang[]>([]);
 
-const toggleSelected = (answer: Answer) => {
-	setSelectedAnswers((prev) => {
-		// Remove any existing answer for this question
-		const filtered = prev.filter(
-			(item) => item.question_title !== answer.question_title
-		);
-		// Add the new selected answer
-		return [...filtered, answer];
-	});
-};
+	// Function Source: reactnative.dev -> docs -> network
+	const getQuizFromApi = () => {
+		return fetch(
+			"https://turva-back-softala-turvallisuus-app.2.rahtiapp.fi/api/quiz/1"
+		)
+			.then((response) => {
+				console.log(response.json());
+				return response.json();
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	const getQuizFromApiAsync = async () => {
+		try {
+			const response = await fetch(
+				"https://turva-back-softala-turvallisuus-app.2.rahtiapp.fi/api/quiz/1"
+			);
+			const json = await response.json();
+			console.log(json);
+			setQuizData(json);
+			return json;
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const quizData1 = getQuizFromApiAsync() as unknown as QuizType;
+	//const quizData = quizData1[lang] as QuizLang;
+
+	const toggleSelected = (answer: Answer) => {
+		setSelectedAnswers((prev) => {
+			// Remove any existing answer for this question
+			const filtered = prev.filter(
+				(item) => item.question_title !== answer.question_title
+			);
+			// Add the new selected answer
+			return [...filtered, answer];
+		});
+	};
 	const isAnswerSelected = (answer: Answer) => {
 		return selectedAnswers.some(
 			(item) =>
@@ -34,15 +63,22 @@ const toggleSelected = (answer: Answer) => {
 				item.title === answer.title
 		);
 	};
-	const isAllAnswered = quizData.questions.every((q) =>
-	selectedAnswers.some((a) => a.question_title === q.title)
-);
 
+	const isAllAnswered = quizData[0].questions.every((q) =>
+		selectedAnswers.some((a) => a.question_title === q.title)
+	);
 
 	return (
-		<ScrollView style={{ backgroundColor: theme.colors.background, marginTop: 20}}>
-			<Text variant="bodyLarge" style={{marginHorizontal: 20, marginBottom: 10}}>{commonText.answerAll}</Text>
-			{quizData.questions.map((question) => (
+		<ScrollView
+			style={{ backgroundColor: theme.colors.background, marginTop: 20 }}
+		>
+			<Text
+				variant="bodyLarge"
+				style={{ marginHorizontal: 20, marginBottom: 10 }}
+			>
+				{commonText.answerAll}
+			</Text>
+			{quizData[0].questions.map((question) => (
 				<View key={question.title} style={styles.answerContainer}>
 					<QuizQuestion
 						title={question.title}
@@ -64,7 +100,7 @@ const toggleSelected = (answer: Answer) => {
 				</View>
 			))}
 			<Button
-				style={{margin: 10, marginBottom: 50}}
+				style={{ margin: 10, marginBottom: 50 }}
 				mode="contained"
 				disabled={!isAllAnswered}
 				onPress={() =>
@@ -75,7 +111,8 @@ const toggleSelected = (answer: Answer) => {
 						},
 					})
 				}
-			>{commonText.end}
+			>
+				{commonText.end}
 			</Button>
 		</ScrollView>
 	);
@@ -86,7 +123,6 @@ const styles = StyleSheet.create({
 		marginBottom: 24,
 		marginHorizontal: 10,
 	},
-
 });
 
 export default Quiz;
